9d48258cd758478a2ac16b676109d3e2b038515a
Socn <xxy780910@gmail.com>
2023-01-29 23:55:50 +0800

    feat: main layout

--- src/components/main.vue
+++ src/components/main.vue
@@ -5,21 +5,29 @@
     <head>
         <title>鹅蛋工作室 | GooseEggGames</title>
     </head>
-    <body>
-        <header>
-            <div class="logo"></div>
-            <div class="actions">
-                <div class="actions-item"></div>
-                <div class="actions-item"></div>
-                <div class="actions-item"></div>
-            </div>
-        </header>
-    </body>
+    <header>
+        <div class="logo"></div>
+        <div class="actions">
+            <div class="actions-item"></div>
+            <div class="actions-item"></div>
+            <div class="actions-item"></div>
+        </div>
+    </header>
+    <div id="body">
+        <content>
+
+        </content>
+
+        <footer>
+            Copyright © 2023 Goose Egg Studio
+        </footer>
+    </div>
 </template>
 
 <style lang="scss">
 header{
-    background-color: rgb(233,233,233);
+    background-color: rgba(233,233,233,0.6);
+    backdrop-filter: blur(20px);
     height: 80px;
     display: flex;
     flex-direction: row;
@@ -28,7 +36,8 @@ header{
     box-sizing: border-box;
     position: fixed;
     top: 0;
-    width: 100%;
+    width: 100vw;
+    z-index: 1000;
     .logo{
         width: 150px;
         height: 50px;
@@ -58,5 +67,46 @@ header{
             flex-basis: 75px;
         }
     }
+}
+
+#body{
+    height: 100vh;
+    overflow-y: scroll;
+    
+}
+#body::-webkit-scrollbar{
+    width: 14px;
+    border-radius: 8px;
+}
+#body::-webkit-scrollbar-thumb {
+    background: rgb(150,150,150); // 滑块颜色
+    border-radius: 8px ; // 滑块圆角
+    border: solid rgb(255,255,255) 3px;
+}
+
+#body::-webkit-scrollbar-thumb:hover {
+    background: rgb(100,100,100); // 鼠标移入滑块变红
+}
+
+#body::-webkit-scrollbar-track {
+    border-radius: 10px; // 轨道圆角
+    background-color: rgb(255,255,255) // 轨道颜色 
+}
+content{
+    position: relative;
+    display: block;
+    width: 100%;
+    height: 200vh;
+}
+
+footer{
+    position: relative;
+    bottom: 0;
+    height: 80px;
+    background-color: rgb(233,233,233);
+    vertical-align: middle;
+    line-height: 80px;
+    text-align: center;
+
 }
 </style>
